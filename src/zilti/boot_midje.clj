a9efe5d31394a431cb0e3a0f4792cb3651008a7c
Juho Teperi <juho.teperi@iki.fi>
2015-01-15 16:19:03 +0200

    unquote splice always needs non-nil list

--- src/zilti/boot_midje.clj
+++ src/zilti/boot_midje.clj
@@ -22,40 +22,27 @@
     (pod/with-eval-in (worker-pods :refresh)
       (midje.repl/load-facts
         ; Double quote each symbol so that when they are unquoted they are still quoted once
-        ~@(if (seq namespaces) (map #(eval `''~%) namespaces))
-        ~@(if (seq filters) (cons :filter filters))
-        ~(if level level)))))
+        ~@(if (seq namespaces) (map #(eval `''~%) namespaces) [])
+        ~@(if (seq filters)    (map (comp eval read-string) filters) [])
+        ~@(if level            [level] [])))))
 
-(defn do-autotest [worker-pods sources filters]
+(defn do-autotest [worker-pods filters]
   (pod/with-eval-in (worker-pods :refresh)
     (midje.repl/autotest
-      :files ~@(if (seq sources) sources (core/get-env :directories))
-      ~@(if (seq filters) [:filter filters]))))
+      :files ~@(core/get-env :directories)
+      ~@(if (seq filters) [:filter filters] []))))
 
 (core/deftask midje
   "Run midje tests in boot."
   [n namespaces NAMESPACE #{sym} "symbols of the namespaces to run tests in."
    a autotest             bool   "Use Midje's built-in autotest."
-   s sources    SOURCE    #{str} "sources to be watched by autotest; both filenames and directory names are accepted."
-   f filters    FILTER    #{str} "midje filters. Only facts matching one or more of the arguments are loaded. The filter arguments are:
-
-                                  :keyword      -- Does the metadata have a truthy value for the keyword?
-                                  \"string\"      -- Does the fact's name contain the given string?
-                                  #\"regex\"      -- Does any part of the fact's name match the regex?
-                                  a function    -- Does the function return a truthy value when given the fact's metadata?"
+   f filters    FILTER    #{str} "midje filters. Only facts matching one or more of the arguments are loaded."
    c config     CONFIG    #{str} "list of midje config files."
-   l level      LEVEL     int    "Set Midje's verbosity level using one of the following options:
-
-                                  :print-normally    (0) -- failures and a summary.
-                                  :print-no-summary (-1) -- failures only.
-                                  :print-nothing    (-2) -- nothing is printed.
-                                  -- (but return value can be checked)
-                                  :print-namespaces  (1) -- print the namespace for each group of facts.
-                                  :print-facts       (2) -- print fact descriptions in addition to namespaces."]
+   l level      LEVEL     int    "Set Midje's verbosity level."]
   (let [worker-pods (pod/pod-pool (update-in (core/get-env) [:dependencies] into pod-deps) :init (partial init config))]
     (core/cleanup (worker-pods :shutdown))
     (core/with-pre-wrap fileset
       (if autotest
-        (do-autotest worker-pods sources filters)
+        (do-autotest worker-pods filters)
         (do-singletest worker-pods namespaces filters level))
       fileset)))
